b75652895a75e2247a0b3947c93aeec416215a1f
leogtzr <leogutierrezramirez@gmail.com>
2020-01-23 23:33:36 -0700

    first commit

---
+++ app-cloud-config-client/src/main/java/com/app/client/AppCloudConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.app.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCloudConfigClientApplication {
+
+	public static void main(final String[] args) {
+		SpringApplication.run(AppCloudConfigClientApplication.class, args);
+	}
+
+}
---
+++ app-cloud-config-client/src/test/java/com/app/client/AppCloudConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCloudConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
