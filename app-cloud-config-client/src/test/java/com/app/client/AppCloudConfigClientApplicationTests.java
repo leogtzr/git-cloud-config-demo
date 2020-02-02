f1906fee52110fb07b30b09ace4dabd4a5441870
leogtzr <leogutierrezramirez@gmail.com>
2020-02-01 18:24:14 -0700

    fix context load test issue

--- app-cloud-config-client/src/test/java/com/app/client/AppCloudConfigClientApplicationTests.java
+++ app-cloud-config-client/src/test/java/com/app/client/AppCloudConfigClientApplicationTests.java
@@ -3,7 +3,7 @@ package com.app.client;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"msg = hola"})
 class AppCloudConfigClientApplicationTests {
 
 	@Test
